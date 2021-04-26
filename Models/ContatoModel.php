e3c40427091913be4e6173c78b9e86a6947af3d2
Gabriel_rosa <gabrielfiamete@icloud.com>
2021-04-26 17:03:30 -0300

    Update ContatoModel.php

--- Models/ContatoModel.php
+++ Models/ContatoModel.php
@@ -6,7 +6,7 @@ class ContatoModel
 {
 
     public static function enviarForm(){
-        $mail = new \Email('smtp.gmail.com', 'gabrielfiamete@gmail.com', '1996160717', 'Gabriel');
+        $mail = new \Email('smtp.gmail.com', '', '', '');
         $mail->addAdress($_POST['mail'], $_POST['nome']);
         $mail->formatarEmail(array('assunto' => 'Mensagem do Site', 'corpo' => $_POST['mensagem']));
         $mail->enviarEmail();
